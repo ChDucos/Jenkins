80285fcd7b20609a9a7b0f9fdfec71a7a1f6ba19
ChDucos <36690032+ChDucos@users.noreply.github.com>
2022-09-10 18:02:55 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
     stages 
-        stage('Stage ') {
+        stage('Stage 1') {
             steps {
                sh ' echo "Hello World - Branche MASTER"'
         }
