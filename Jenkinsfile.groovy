65384b1b9bebbcccd096b333f55308b13e726961
ChDucos <36690032+ChDucos@users.noreply.github.com>
2022-09-10 16:31:37 +0200

    Add files via upload

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages 1
         stage('Stage ') {
             steps {
-                echo "Hello World"'
+                echo "Hello World"
                 
                     echo "Multiline shell steps works too"
                  
